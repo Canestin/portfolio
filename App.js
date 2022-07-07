dc83fca59e50e7ce5be1020eaa1a6d3ac5fc87ac
Canestin <pierre.ndong-ngoua@eleve.isep.fr>
2022-07-07 12:29:48 +0200

    Update

--- App.js
+++ App.js
@@ -4,13 +4,18 @@ const buttonLang = document.querySelector('button#lang');
 const text = document.querySelector('.container h1');
 const logo = document.querySelector('#logo');
 const imgLogos = document.querySelectorAll('.icones div img');
+const menu = document.querySelector('#menu');
+const headerMobile = document.querySelector('.headerMobile');
 
+
+// Scroll Header Beginning !
 
 function headerScroll(){
         myHeader.style.backgroundColor = "#5bcba7";  // vert
-        myHeader.style.paddingTop = "0";
+        myHeader.style.paddingTop = "0px";
         buttonLang.style.border = "3px solid #050a30";   // noir
-        buttonLang.style.color = "#050a30";
+        buttonLang.style.color = "#5bcba7";
+        buttonLang.style.backgroundColor = "#050a30";
         logo.src = "../img/logo-noir.png";
         for(let i=0; i < linkNavbar.length; i++){
             linkNavbar[i].style.color = "#050a30";
@@ -18,32 +23,34 @@ function headerScroll(){
 }
 
 function headerNoScroll(){
-    myHeader.style.backgroundColor = "transparent";   //Normal
+    myHeader.style.backgroundColor = "transparent";
     myHeader.style.paddingTop = "70px";
     buttonLang.style.border = "3px solid #5bcba7";
     buttonLang.style.color = "#5bcba7";
+    buttonLang.style.backgroundColor = "#050a30";
     logo.src = "../img/logo-blanc.png";
     for(let i=0; i < linkNavbar.length; i++){
         linkNavbar[i].style.color = "#5bcba7";
     }
 }
 
-window.addEventListener("scroll", () => {
-    if(window.scrollY > 70){
-        headerScroll();
-    } else {
-        headerNoScroll();
-    }
-})
+if(window.innerWidth > 798){
+    window.addEventListener("scroll", () => {
+        if(window.scrollY > 20){
+            headerScroll();
+        } else {
+            headerNoScroll();
+        }
+    })
+    window.addEventListener("load", () => {
+        if(window.scrollY > 20){
+            headerScroll();
+        }
+    });
+}
 
-window.addEventListener("load", () => {
-    setInterval(turnImages, 6300);
-    if(window.scrollY > 70){
-        headerScroll();
-    }
-});
+// Scroll Header End !
 
-turnImages();
 
 function turnImages(){
     for(let i=0; i < imgLogos.length; i++){
@@ -52,7 +59,7 @@ function turnImages(){
     
     function animLogos(i) {
         imgLogos[i].animate([
-            { transform: 'rotate3d( 1, 2, 3, 360deg)'}
+            { transform: 'rotate3d(1, 2, 3, 360deg)'}
           ], {
             duration: 700,
             iterations: 1,
@@ -60,3 +67,7 @@ function turnImages(){
         });     
     }
 }
+
+window.addEventListener("load", () => {
+    turnImages();
+});
